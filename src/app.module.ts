4515bf4f0371dc5e3636bc8c8192b65f4c7f5555
Matheus Nunes Cerutti <matheuscerutti96@gmail.com>
2023-09-04 15:00:54 -0300

    Criacao dos modulos e config prisma

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { HealthModule } from './health/health.module';
+import { CardsModule } from './cards/cards.module';
+import { SafeNotesModule } from './safe_notes/safe_notes.module';
+import { EraseModule } from './erase/erase.module';
+import { CredentialsModule } from './credentials/credentials.module';
+
+@Module({
+  imports: [PrismaModule, UsersModule, HealthModule, CardsModule, SafeNotesModule, EraseModule, CredentialsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
